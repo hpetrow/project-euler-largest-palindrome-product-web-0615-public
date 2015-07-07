8c7a69834f0f76316bc2ed5ee0eccfacbf342d81
hpetrow <heather.petrow@gmail.com>
2015-07-07 10:26:11 -0400

    done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  max_product = 0
+
+  999.downto(100) { |n1|
+    999.downto(100) { |n2|
+      product = n1 * n2
+      if isPalindrome(product) && product > max_product
+        max_product = product
+      end
+    }
+  }
+  max_product
+end
+
+def isPalindrome(n)
+  n.to_s == n.to_s.reverse
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def answer
+    max_product = 0
+
+    999.downto(100) { |n1|
+      999.downto(100) { |n2|
+        product = n1 * n2
+        if self.isPalindrome(product) && product > max_product
+          max_product = product
+        end
+      }
+    }
+    max_product
+  end
+
+  def isPalindrome(n)
+    n.to_s == n.to_s.reverse
+  end
+end
